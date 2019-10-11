e48ac0ec1b7fa969e41ba17d09a4c3b8bfda0f2c
Soporte Taxis Ya <soportetaxisya@iMac-de-Soporte.local>
2019-10-11 11:50:30 -0500

    ajuste

--- config/s3.env.js
+++ config/s3.env.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const env = {
-    AWS_ACCESS_KEY: 'AKIA2IA7FDEN3PC3BD5J',
-    AWS_SECRET_ACCESS_KEY: '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',
+    AWS_ACCESS_KEY: '',
+    AWS_SECRET_ACCESS_KEY: '',
     REGION: 'us-east-2',
     Bucket: 'usbimage'
 };
